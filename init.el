c9a6ec2b8460b0271a9cfab8c9eaec2ceef3e70d
crepefan <efeerr08@gmail.com>
2021-11-07 15:51:56 +0300

    Some optimization

--- init.el
+++ init.el
@@ -1,28 +1,32 @@
-;; Get rid of annoying stuff
-(setq custom-file        (concat user-emacs-directory "custom.el")
-      make-backup-files  nil
-      ring-bell-function 'ignore)
-
-(menu-bar-mode   -1)
-(tool-bar-mode   -1)
-(scroll-bar-mode -1)
-
-;; Relative line numbers
-(global-display-line-numbers-mode 1)
-(setq display-line-numbers-type 'relative)
-
-;; Load config
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-
-(require 'init-use-package)
-
-(require 'init-path)
-(require 'init-evil)
-
-(require 'init-ivy)
-
-(require 'init-theme)
-(require 'init-dashboard)
-
-(require 'init-lsp)
-(require 'init-rust)
+;; Optimization
+(let ((file-name-handler-alist nil))
+  (setq gc-cons-threshold 100000000)
+  
+  ;; Get rid of annoying stuff
+  (setq custom-file        (concat user-emacs-directory "custom.el")
+	make-backup-files  nil
+	ring-bell-function 'ignore)
+  
+  (menu-bar-mode   -1)
+  (tool-bar-mode   -1)
+  (scroll-bar-mode -1)
+  
+  ;; Relative line numbers
+  (global-display-line-numbers-mode 1)
+  (setq display-line-numbers-type 'relative)
+  
+  ;; Load config
+  (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+  
+  (require 'init-use-package)
+  
+  (require 'init-path)
+  (require 'init-evil)
+  
+  (require 'init-ivy)
+  
+  (require 'init-theme)
+  (require 'init-dashboard)
+  
+  (require 'init-lsp)
+  (require 'init-rust))
