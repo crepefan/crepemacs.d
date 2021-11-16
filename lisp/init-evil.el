bfa26dae013be410b2ad0660a7f5e31a0897f0f8
crepefan <efeerr08@gmail.com>
2021-11-16 15:56:18 +0300

    Remove evil-escape

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -6,10 +6,4 @@
   (evil-mode 1)
   (define-key evil-motion-state-map (kbd "SPC") nil))
 
-(use-package evil-escape
-  :ensure t
-  :init
-  (evil-escape-mode 1)
-  (setq-default evil-escape-key-sequence "jj"))
-
 (provide 'init-evil)
