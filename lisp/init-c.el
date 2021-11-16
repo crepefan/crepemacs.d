4fd908100b37e2f6042b4bf4c9d11564cf735d2e
crepefan <efeerr08@gmail.com>
2021-11-16 18:20:42 +0300

    Add C++ support

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -1,6 +1,7 @@
 ;; init-c.el -- Configuration related to C language
 
 (add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
 (add-hook 'makefile-mode-hook (lambda () (setq tab-width 2)))
 
 (provide 'init-c)
