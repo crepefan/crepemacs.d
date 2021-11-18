afb557801716ee130586758a92ce64b3c6b6299c
crepefan <efeerr08@gmail.com>
2021-11-19 02:21:14 +0300

    Fix Haskell

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -8,8 +8,7 @@
 	 (haskell-mode . (lambda ()
 			   (define-key evil-normal-state-local-map (kbd "SPC hi") 'haskell-interactive-bring))))
   :config
-  (setq lsp-haskell-server-path "/usr/bin/haskell-language-server"
-	lsp-haskell-formatting-provider 'ormolu))
+  (setq lsp-haskell-server-path "/usr/bin/haskell-language-server"))
 
 (use-package lsp-haskell
   :ensure t
